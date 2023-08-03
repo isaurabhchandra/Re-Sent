d59bd8de1b5e6912a81e9b34e2256aa2b11862de
Saurabh Chandra <chandrasaurabh0019@gmail.com>
2023-08-03 11:15:16 +0530

    firebase initialize

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,14 @@
+ import firebase from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyAym1ai__rxCO_1NsDY9bEC1LgFB06Csag",
+    authDomain: "resent-6d4fa.firebaseapp.com",
+    databaseURL: "https://resent-6d4fa-default-rtdb.firebaseio.com",
+    projectId: "resent-6d4fa",
+    storageBucket: "resent-6d4fa.appspot.com",
+    messagingSenderId: "955922840444",
+    appId: "1:955922840444:web:849ac67feeac6d0f2bc2f9",
+    measurementId: "G-0GFLS6L34D"
+  };
+
+  const app = firebase.initializeApp(config);
