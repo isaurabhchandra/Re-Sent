451465f9cbc619082713c25bcc0e4a8252cf5082
Saurabh Chandra <chandrasaurabh0019@gmail.com>
2023-08-08 10:47:45 +0530

    upadated nickname

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,17 +1,28 @@
-import { Button, Icon, Drawer } from 'rsuite';
-import { useModelState } from '../../misc/custom-hook';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
+import { useMediaQuery, useModelState } from '../../misc/custom-hook';
 import Dashboard from './MainDashboard';
+import { useCallback } from 'react';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModelState();
+  const isMobile = useMediaQuery('(max-width:992px)')
+
+  const onSignOut = useCallback(()=>{
+    auth.signOut();
+    Alert.info('Sign Out', 5000)
+
+    close();
+
+  },[close])
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" />
         Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+      <Drawer full ={isMobile} show={isOpen} onHide={close} placement="left">
+        <Dashboard onSignOut ={onSignOut} />
       </Drawer>
     </>
   );
