ec8633c72729abac50284db647006af0672aa4ed
Saurabh Chandra <chandrasaurabh0019@gmail.com>
2023-08-08 12:13:07 +0530

    linked to google and facebook

--- src/components/dashboard/MainDashboard.jsx
+++ src/components/dashboard/MainDashboard.jsx
@@ -2,6 +2,7 @@ import { Button, Divider, Drawer,Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import EditableInput from '../EditableInput';
 import { database } from '../../misc/firebase';
+import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({onSignOut}) => {
   const { profile } = useProfile();
@@ -29,6 +30,7 @@ const Dashboard = ({onSignOut}) => {
       
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <ProviderBlock/>
         <Divider/>
         <EditableInput
         name = 'nickname'
