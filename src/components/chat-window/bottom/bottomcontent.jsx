edb0ecf41f16598587b08ad7c300b7c6bb5658e6
Saurabh Chandra <chandrasaurabh0019@gmail.com>
2023-08-13 19:55:11 +0530

    Created description model

---
+++ src/components/chat-window/bottom/bottomcontent.jsx
@@ -0,0 +1,11 @@
+
+
+const Bottom = () => {
+  return (
+    <div>
+      bottom
+    </div>
+  )
+}
+
+export default Bottom
--- src/page/Home/Chat.jsx
+++ src/page/Home/Chat.jsx
@@ -1,11 +1,45 @@
 
+import { useParams } from 'react-router';
+import { Loader } from 'rsuite';
+import Top from '../../components/chat-window/top/topcontent';
+import Message from '../../components/chat-window/message/message';
+import Bottom from '../../components/chat-window/bottom/bottomcontent';
+import { useRooms } from '../../context/Room.context';
+import { CurrentRoomProvider } from '../../context/current-roomcontext';
 
-function Chat() {
+const Chat = () => {
+  const { chatId } = useParams();
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
+  }
+
+  const { name, description } = currentRoom;
+  const currentRoomData = {
+    name,
+    description,
+  };
+
   return (
-    <div>
-      Hello
-    </div>
-  )
-}
+    <CurrentRoomProvider data={currentRoomData}>
+      <div className="chat-top">
+        <Top />
+      </div>
+      <div className="chat-middle">
+        <Message />
+      </div>
+      <div className="chat-bottom">
+        <Bottom />
+      </div>
+    </CurrentRoomProvider>
+  );
+};
 
-export default Chat
+export default Chat;
