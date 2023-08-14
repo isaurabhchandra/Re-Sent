e883735b9b7aaf3087de6abf17c55d491a9313bf
Saurabh Chandra <chandrasaurabh0019@gmail.com>
2023-08-14 12:37:31 +0530

    Display user Profile

--- src/components/chat-window/message/MessageItems.jsx
+++ src/components/chat-window/message/MessageItems.jsx
@@ -1,26 +1,30 @@
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
 import TimeAgo from 'timeago-react';
+import ProfileInfoBtn from './ProfileInfoBtn';
+
 const MessageItems = ({ message }) => {
   const { author, createdAt, text } = message;
+
   return (
     <li className="padded mb-1">
-      <div className="d-flex align-item-center font-border mb-1">
+      <div className="d-flex align-items-center font-border mb-1">
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
           className="ml-1"
           size="xs"
         />
-        <span className='ml-2'>{author.name}</span>
+       
+        <ProfileInfoBtn
+          profile={author}
+          appearance="link"
+          classNamep="p-0 ml-1 text-black"
+        />
         <TimeAgo
-         datetime={
-          createdAt  // Changed 'DataTransfer' to 'Date'
-          }
+          datetime={createdAt}
           className="font-normal text-black-65 ml-2"
-        
         />
       </div>
-
       <div>
         <span className="word-breal-all">{text}</span>
       </div>
